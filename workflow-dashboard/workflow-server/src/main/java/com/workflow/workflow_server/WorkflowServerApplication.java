f968e21a310aee34b49dcc44ce918254011a3b07
jaya.chakri2005 <jaya.chakri2005@gmail.com>
2025-06-20 05:35:03 +0000

    Workflow System

---
+++ workflow-dashboard/workflow-server/src/main/java/com/workflow/workflow_server/WorkflowServerApplication.java
@@ -0,0 +1,13 @@
+package com.workflow.workflow_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowServerApplication.class, args);
+	}
+
+}
---
+++ workflow-dashboard/workflow-server/src/test/java/com/workflow/workflow_server/WorkflowServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workflow.workflow_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
